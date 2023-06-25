3b9dd74f5d30dd23b5f2a8e6f05dff9bfe4726fd
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-06-26 04:45:23 +0530

    connected tags

--- logic/dashboard_script.js
+++ logic/dashboard_script.js
@@ -18,8 +18,103 @@ function searchDocuments() {
       }
     }
   }
+  const documentCard = (title, author_name, description,id)=>{
+     
+    return (
+      `
+      <div target="_blank" class="document-card" id = ${id}>
+      <h2 class="document-name">${title}</h2>
+      <p class="document-author">Author: ${author_name}</p>
+      <p class="document-description">
+        ${description}
+      </p>
+    </div>
+      `
+    )
+  }
+
+const get_all_tagged_documents = async(token)=>{
+  fetch('http://localhost:8000/api/document/tagged_doc',{
+      mode: 'cors',
+      method: 'GET',
+      headers: {
+         'authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+     }}).then(response => {
+     
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Login failed');
+      }
+    }).then(info => {
+     const {data} = info;
+   
+    for(let i = 0; i < data.length; i++){
+    
+         const {title,description} = data[i].document_id;
+       
+         const author_name = data[i].document_id.createdBy.name;
+         
+         const doc_id = data[i].document_id.createdBy._id;
+    const card = documentCard(title,author_name,description,doc_id);
+  //  here card is just string and hence we can't access its id property
+  // it is not a dom element
   
-  
+    let cardContainer = document.getElementById('tagged-documents');
+    console.log(cardContainer);
+    cardContainer.innerHTML += card;
+    }
+      })
+    }
+   
+
+const get_all_created_documents = async(token)=>{
+  fetch('http://localhost:8000/api/document/show_created_doc',{
+      mode: 'cors',
+      method: 'GET',
+      headers: {
+         'authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+     }}).then(response => {
+     
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Login failed');
+      }
+    }).then(info => {
+      const {data} = info;
+     
+      for(let i = 0; i < data.length; i++){
+    
+        const {title,description} = data[i];
+        const author_name = data[i].createdBy.name;
+      
+        const doc_id = data[i]._id;
+    const card = documentCard(title,author_name,description,doc_id);
+    const cardContainer = document.getElementById('created-documents');
+    
+    cardContainer.innerHTML += card;  
+       
+      }
+    })
+  }
+
+
+
+const token = JSON.parse(localStorage.getItem('responseData')).token;
+
+
+
+get_all_tagged_documents(token);
+get_all_created_documents(token);
+
+
+
+
+
+
   // Handle click event on the window to clear the dropdown
   window.addEventListener("click", function (event) {
     var dropdown = document.getElementById("searchDropdown");
